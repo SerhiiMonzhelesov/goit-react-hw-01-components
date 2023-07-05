4238b3e4a4022c4cba0a02f299540c4d07075af6
SerhiiMonzhelesov <monzhelesov@gmail.com>
2023-07-06 00:03:03 +0300

    a

--- src/components/Friend/Friend.jsx
+++ src/components/Friend/Friend.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <li className="item">
-      <span className="status">{isOnline ? 'online' : 'nemae'}</span>
+      <span className="status">{isOnline ? "😁" : "😒"}</span>
       <img className="avatar" src={avatar} alt={name} width="48" />
       <p className="name">{name}</p>
     </li>
