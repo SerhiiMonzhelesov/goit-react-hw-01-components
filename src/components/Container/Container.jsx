de223db592e0444d67d2022c539e162212525a3a
SerhiiMonzhelesov <monzhelesov@gmail.com>
2023-07-09 19:23:49 +0300

    add props container

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,7 +1,14 @@
-import { StyledContainer } from "./StyledContainer";
+import { StyledContainer } from './StyledContainer';
+import PropTypes from 'prop-types';
 
 function Container({ children, ...restProps }) {
-    return <StyledContainer {...restProps}>{children}</StyledContainer>
+  return <StyledContainer {...restProps}>{children}</StyledContainer>;
 }
+
+Container.propTypes = {
+    children: PropTypes.arrayOf(
+        PropTypes.element.isRequired
+    ).isRequired
+};
 
 export default Container;
